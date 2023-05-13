814c20c53c4dc9fbdad5438f6262a50105f7fd2c
sea-yassan33 <a.yasuda_tr51@me.com>
2023-05-13 13:37:56 +0900

    RestController2

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -4,18 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @RestController
 public class Sample1appApplication {
-	
+	String[][] data = {
+		{"noname","no email address","0"},
+		{"taro","taro@sample.com","39"},
+		{"hanako","hanako@sample.com","28"},
+		{"sachiko","sachiko@sample.com","17"},
+		{"jiro","jiro@change","6"}
+	};
 	public static void main(String[] args) {
 		SpringApplication.run(Sample1appApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String index(){
-		return "Hello, Spring Boot 3";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num){
+		int n = num < 0 ? 0 :num >= data.length ? 0 : num;
+		String[] item = data[n];
+		String msg = "ID=%s. {name: %s, mail: %s, age: %s}";
+		return String.format(msg, num, item[0], item[1],item[2]);
 	}
-
 }
