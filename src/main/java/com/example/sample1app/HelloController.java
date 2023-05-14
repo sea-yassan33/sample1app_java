c1350803f08ee7573971e3e77a6db5321711bcdd
sea-yassan33 <a.yasuda_tr51@me.com>
2023-05-14 12:03:29 +0900

    Thymeleaf1

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -3,16 +3,14 @@ package com.example.sample1app;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{temp}")
-    public String index(@PathVariable String temp) {
-        switch (temp) {
-            case "index":
-                return "index";
-            default:
-                return "other";
-        }
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg",
+                "これはcontrollerに用意したメッセージです");
+        return "index";
     }
 }
