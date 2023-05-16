b46b19d6fffedb184d3ae3ad72440613c5155016
sea-yassan33 <a.yasuda_tr51@me.com>
2023-05-16 20:29:37 +0900

    Thymeleaf6

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -12,35 +12,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(ModelAndView mav){
-        mav.addObject("msg","フォームを送信して下さい");
         mav.setViewName("index");
+        mav.addObject("msg","HelloController/indexのページです。");
         return mav;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ModelAndView form(
-            @RequestParam(value = "check1", required = false)boolean check1,
-            @RequestParam(value = "radio1", required = false)String radio1,
-            @RequestParam(value = "select1", required = false)String select1,
-            @RequestParam(value = "select2", required = false)String[] select2,
-            ModelAndView mav){
-        String res = "";
-        try {
-            res = "check:" + check1 +
-                    " radio:" + radio1 +
-                    " select:" + select1 +
-                    "\nselect2:" + select2;
-        } catch (NullPointerException e) {}
-        try {
-            res += select2[0];
-            for(int i = 1;i < select2.length;i++) {
-                res += ", " + select2[i];
-            }
-        } catch (NullPointerException e) {
-            res += "null";
-        }
-        mav.addObject("msg", res);
-        mav.setViewName("index");
-        return mav;
+    @RequestMapping("/other")
+    public String other(){
+        return "redirect:/";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "forward:/";
     }
 }
